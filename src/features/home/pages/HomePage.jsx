80e409cac76ad39151d7c3ec094ff17bdff1ecc0
Southern <nhatnam13112005@gmail.com>
2025-09-24 01:05:29 +0700

    (FIX)[SWP391-67]  Resolve search bar z-index issue

--- src/features/home/pages/HomePage.jsx
+++ src/features/home/pages/HomePage.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react'
 
 function HomePage() {
   const [isFixed, setIsFixed] = useState(false)
+
   useEffect(() => {
     const handleScroll = () => {
       const scrollPosition = window.scrollY
@@ -21,37 +22,32 @@ function HomePage() {
 
   return (
     <main>
-      <div className="relative h-[calc(100vh-7.5rem)] w-full bg-[url('/backgroundHero.webp')] bg-cover bg-center opacity-80">
-        {/* search bar */}
+      <div className='relative h-[calc(100vh-7.5rem)] w-full'>
+        <div className="absolute inset-0 bg-[url('/backgroundHero.webp')] bg-cover bg-center" />
+        <div className='absolute inset-0 bg-black/30' />
         <div
-          className={`grid grid-cols-11 place-items-center rounded-lg bg-white p-4 shadow-md transition-all duration-300 ease-in-out ${isFixed ? 'fixed top-0 left-0 w-full rounded-none' : 'absolute top-4 left-1/2 w-[80vw] -translate-x-1/2'}`}
+          className={`z-10 container ${isFixed ? 'fixed top-0 left-1/2 -translate-x-1/2' : 'absolute top-4 left-1/2 -translate-x-1/2'}`}
         >
           {/*-translate-x-1/2 shift back half by its own */}
-          <div className='col-span-3'>
-            <p>Địa điểm nhận xe</p>
-            <p>Chọn địa điểm nhận xe</p>
+          <div className='bg-background grid grid-cols-11 place-items-center rounded-lg p-4 shadow-md transition-all duration-300 ease-in-out'>
+            <div className='col-span-3'>
+              <p>Địa điểm nhận xe</p>
+              <p>Chọn địa điểm nhận xe</p>
+            </div>
+            <div className='col-span-3'>
+              <p>Ngày nhận xe</p>
+              <p>Giờ nhận xe</p>
+            </div>
+            <div className='col-span-3'>
+              <p>Ngày trả xe</p>
+              <p>Giờ trả xe</p>
+            </div>
+            <Button className='col-span-2 h-12 w-full cursor-pointer bg-[#51C09F] hover:bg-[#51C09F]/80'>
+              TÌM XE
+            </Button>
           </div>
-          <div className='col-span-3'>
-            <p>Ngày nhận xe</p>
-            <p>Giờ nhận xe</p>
-          </div>
-          <div className='col-span-3'>
-            <p>Ngày trả xe</p>
-            <p>Giờ trả xe</p>
-          </div>
-          <Button className='col-span-2 h-12 w-full cursor-pointer bg-[#51C09F] hover:bg-[#51C09F]/80'>
-            TÌM XE
-          </Button>
         </div>
       </div>
-      <div>Noi dung tiep theo</div>
-      <div>Noi dung tiep theo</div>
-      <div>Noi dung tiep theo</div>
-      <div>Noi dung tiep theo</div>
-      <div>Noi dung tiep theo</div>
-      <div>Noi dung tiep theo</div>
-      <div>Noi dung tiep theo</div>
-      <div>Noi dung tiep theo</div>
     </main>
   )
 }
