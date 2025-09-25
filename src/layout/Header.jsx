a21d4f431282849cd70a5a3938ca47eebab33118
Southern <nhatnam13112005@gmail.com>
2025-09-25 13:42:08 +0700

    (FEAT)[SWP391-69] Implement Footer Layout and Update Header layout with logo

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -6,9 +6,9 @@ import { Link } from 'react-router-dom'
 const Header = () => {
   return (
     <header className='flex h-30 items-center border-b'>
-      <div className='container mx-auto flex justify-between px-4'>
-        <Link to='/' className='cursor-pointer text-3xl'>
-          Logo
+      <div className='container mx-auto flex justify-between pr-4'>
+        <Link to='/'>
+          <img src='/logo.svg' alt='e-Motion' className='h-[80px] w-[150px] cursor-pointer' />
         </Link>
         <nav className='flex items-center space-x-4'>
           <Link to='/cars'>Xem danh sách xe</Link>
