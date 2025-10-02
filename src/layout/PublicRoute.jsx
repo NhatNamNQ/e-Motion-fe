16414e7e8276efb931b9838ee9da1f567d1fa3ef
Southern <nhatnam13112005@gmail.com>
2025-10-02 08:18:39 +0700

    (FEAT)[SWP391-106] Implement Protected and Public Routes

---
+++ src/layout/PublicRoute.jsx
@@ -0,0 +1,12 @@
+import { useAuth } from '@/hooks/useAuth'
+import { Navigate } from 'react-router-dom'
+
+const PublicRoute = ({ children }) => {
+  const { isAuthenticated, isLoading } = useAuth()
+  if (isLoading) return <div>Loading ...</div>
+  if (isAuthenticated) return <Navigate to='/' replace />
+
+  return children
+}
+
+export default PublicRoute
